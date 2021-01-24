f54cd82aee55fcd0438ca64855ccd7e8578181c5
mengmli <mamieoswin@gmail.com>
2021-01-24 13:20:55 +0100

    rewrite 1

---
+++ main.m
@@ -0,0 +1,13 @@
+% for executing all the subfunctions and producing final plots
+
+% main workflow for the code:
+
+%{
+    I. Sampling & Preprocessing
+        1. read the generated samples from predefined markov chain dynamics
+        2. estimate the transition kernels from the generated samples
+    II. FW step
+        return the predictors given a prescriptor value
+    III. Plot
+}
+
