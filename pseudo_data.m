bbf7dc6006e0c322131c6f84ec7e11510f961c78
mengmli <mamieoswin@gmail.com>
2021-01-25 15:09:12 +0100

    pseudo data and estimation

---
+++ pseudo_data.m
@@ -0,0 +1,37 @@
+rng(1);
+% parameter setting
+
+k=10; % how many customer segements: i.e., how many different markov chain dynamics
+d=12; % how many brands
+T=30; % length of each xi^(i)/ sample size
+n_exper = 10; % number of independent experiments
+
+for n=1:n_exper
+
+    P=rand(d,d,k); % k d*d transition matrix
+    for i=1:k
+        P(:,:,i)=P(:,:,i)./sum(P(:,:,i),2); %normalize to standard row-stochastic matrix
+        mc=dtmc(P(:,:,i));
+        while (isergodic(mc)==0) && (isreducible(mc)==1) % keep rolling the dice until getting an ergodic and irreducible chain dynamics
+            P(:,:,i)=rand(d,d);
+            P(:,:,i)=P(:,:,i)./sum(P(:,:,i),2); %normalize to standard row-stochastic matrix
+            mc=dtmc(P(:,:,i));
+        end
+    end
+
+    % generating pseudo-data from predefined markov chain dynamics: Random Walk
+
+    xi = rand(k,T); % for each customer segment, generate a different trajectory with regard to its jumping dynamics
+    for i=1:k
+        X=simulate(dtmc(P(:,:,i)),T-1);
+        xi(i,:)=X';
+    end
+    xi=reshape(xi.',1,[]); %flatten xi matrix
+
+    if n==1
+        save('xi_new.mat','xi');
+        matObj = matfile('xi_new.mat','Writable',true); %save
+    else
+        matObj.xi(n,:)=xi; % otherwise append
+    end
+end
