cdbdd34b679f0e852a2caa0e7c8e4fd5ee32c061
mengmli <mamieoswin@gmail.com>
2021-01-26 09:46:37 +0100

    gradient formula

--- grad_psi.m
+++ grad_psi.m
@@ -1,4 +1,3 @@
-
 function val0 = Grad_Psi(L,P)
     % P input TRANSITION matrix
     % L: vector of constant that appears in the loss function: i.e., Psi= sum(L(x,i)*pi_i,i)
@@ -8,10 +7,11 @@ function val0 = Grad_Psi(L,P)
     % first we compute d pi/ d P using the formula
     % to do that we use the fundamental matrix of ergodic MC: Z
     mc=dtmc(P);
-    pi_0=asymptotic
+    pi_0=asymptotics(mc); % stationary distribution (row vector)
+    pi_0=pi_0';
     d = length(P); %size of the matrix
-    Z=(eye(d)-P+)
-    d_pi = ()
-
+    Z=inv(eye(d)-P+pi_0*ones(1,d)); % fundamental matrix formula
+    d_pi = kron(pi_0',(Z-pi_0*ones(1,d))); % the given formula
+    val0=L*d_pi; % dot product following from the chain rule
         
 end
